987da2ce9487788f374bc37bbb674e4a992e3dc9
abdallah-alaff96 <abdallah96_alaff@hotmail.com>
2022-02-01 21:39:54 +0200

    finish the App (filter list with checkbox)-final

--- src/Images.js
+++ src/Images.js
@@ -1,5 +1,34 @@
+import { useEffect, useState } from "react";
+
 const Images = () => {
-  return <div>hi</div>;
+  const [productsArr, setProductsArr] = useState([]);
+
+  // fetch function
+  const fetchImgs = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    if (!response.ok) {
+      throw new Error("Something went wrong, no fetched data!!");
+    }
+    const dataArr = await response.json();
+    setProductsArr(dataArr);
+    console.log(dataArr);
+  };
+
+  useEffect(() => {
+    fetchImgs();
+  }, []);
+
+  const content = productsArr.map((product) => {
+    return (
+      <div key={product.id}>
+        <h5>{product.title}</h5>
+        <p>{product.price}</p>
+        <img src={product.image} />
+      </div>
+    );
+  });
+
+  return <>{content}</>;
 };
 
 export default Images;
